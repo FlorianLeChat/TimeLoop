7b74949cf3857a6f6d8f64c6a1e4240c7c0bc7c2
Florian Trayon <git@florian-trayon.fr>
2025-07-27 20:15:54 +0200

    Added Svelte components to generate a two-date countdown

---
+++ src/components/SaveButton.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	// Importation des dépendances et composants.
+	import { SvelteURLSearchParams } from "svelte/reactivity";
+
+	// Initialisation des variables.
+	let {
+		startDate = $bindable( "" ),
+		endDate = $bindable( "" ),
+		startIsNow = $bindable( false ),
+		endIsNow = $bindable( false )
+	}: {
+		startDate: string;
+		endDate: string;
+		startIsNow: boolean;
+		endIsNow: boolean;
+	} = $props();
+
+	// Fonction de sauvegarde de l'URL dans la barre d'adresse
+	//  avec copie dans le presse-papiers
+	const saveToURL = async () =>
+	{
+		const parameters = new SvelteURLSearchParams( window.location.search );
+		parameters.set( "start", startIsNow ? "now" : startDate );
+		parameters.set( "end", endIsNow ? "now" : endDate );
+
+		const url = `${ window.location.origin }${ window.location.pathname }?${ parameters.toString() }`;
+		history.replaceState( null, "", url ); // Remplacement de l'URL actuelle sans recharger la page.
+
+		try
+		{
+			await navigator.clipboard.writeText( url );
+			alert( "URL copied to clipboard!" );
+		}
+		catch ( error )
+		{
+			console.error( "Failed to copy URL:", error );
+		}
+	};
+</script>
+
+<button
+	class="mt-4 px-4 py-2 bg-blue-600 hover:bg-blue-700 rounded text-white text-sm"
+	onclick={saveToURL}
+>
+	Save current countdown
+</button>
---
+++ src/components/TimeUnit.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	// Importation des dépendances et composants.
+	import NumberFlow from "@number-flow/svelte";
+
+	// Initialisation des variables.
+	let {
+		label = $bindable( "" ),
+		value = $bindable( 0 )
+	}: {
+		label: string;
+		value: number;
+	} = $props();
+</script>
+
+<div class="flex flex-col items-center">
+	<NumberFlow trend={0} {value} format={{ minimumIntegerDigits: 2 }} />
+
+	<span class="text-sm mt-1">{label}</span>
+</div>
