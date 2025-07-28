2ab2c89bbfe53ab716edd4ff97f7c42a776b4f19
Florian Trayon <git@florian-trayon.fr>
2025-07-28 08:23:46 +0200

    Fixed accent colours on countdown display settings

--- src/components/SettingsPanel.svelte
+++ src/components/SettingsPanel.svelte
@@ -24,33 +24,59 @@
 		<input
 			id="showCenturies"
 			type="checkbox"
+			class="accent-blue-600 mr-1"
 			bind:checked={showCenturies}
 		/>
 		<label for="showCenturies">Show centuries</label>
 	</li>
 
 	<li>
-		<input id="showYears" type="checkbox" bind:checked={showYears} />
+		<input
+			id="showYears"
+			type="checkbox"
+			class="accent-blue-600 mr-1"
+			bind:checked={showYears}
+		/>
 		<label for="showYears">Show years</label>
 	</li>
 
 	<li>
-		<input id="showDays" type="checkbox" bind:checked={showDays} />
+		<input
+			id="showDays"
+			type="checkbox"
+			class="accent-blue-600 mr-1"
+			bind:checked={showDays}
+		/>
 		<label for="showDays">Show days</label>
 	</li>
 
 	<li>
-		<input id="showHours" type="checkbox" bind:checked={showHours} />
+		<input
+			id="showHours"
+			type="checkbox"
+			class="accent-blue-600 mr-1"
+			bind:checked={showHours}
+		/>
 		<label for="showHours">Show hours</label>
 	</li>
 
 	<li>
-		<input id="showMinutes" type="checkbox" bind:checked={showMinutes} />
+		<input
+			id="showMinutes"
+			type="checkbox"
+			class="accent-blue-600 mr-1"
+			bind:checked={showMinutes}
+		/>
 		<label for="showMinutes">Show minutes</label>
 	</li>
 
 	<li>
-		<input id="showSeconds" type="checkbox" bind:checked={showSeconds} />
+		<input
+			id="showSeconds"
+			type="checkbox"
+			class="accent-blue-600 mr-1"
+			bind:checked={showSeconds}
+		/>
 		<label for="showSeconds">Show seconds</label>
 	</li>
 </ul>
